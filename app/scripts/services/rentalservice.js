cc4a07676b99e165c59682d00c09892db5007674
Tomo Masakura <fj0706kr@aa.jp.fujitsu.com>
2015-07-20 15:06:53 +0900

    旧作の料金計算ロジックを追加

--- app/scripts/services/rentalservice.js
+++ app/scripts/services/rentalservice.js
@@ -11,8 +11,24 @@ angular.module('app7App')
   .factory('rentalService', function () {
     var rentalService = {
       getRentalFee: function (item, days) {
-        // ひとまずすべて新作で計算する
-        return days * 300;
+        if (item.type === 'new') {
+          // 新作
+
+          // 一日毎に 300 円
+          return days * 300;
+        } else if (item.type === 'regular') {
+          // 旧作
+
+          // 二日目までは 200 円
+          var amount = 200;
+
+          // 三日目からは一日毎に 150 円
+          if (days > 2) {
+            amount += (days - 2) * 150;
+          }
+
+          return amount;
+        }
       }
     };
 
