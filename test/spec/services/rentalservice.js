74a6ecea9e5d43c4068814821d51343342e5f8f5
Tomo Masakura <fj0706kr@aa.jp.fujitsu.com>
2015-07-20 15:03:38 +0900

    レンタル料計算のテストコードを追加

--- test/spec/services/rentalservice.js
+++ test/spec/services/rentalservice.js
@@ -16,4 +16,30 @@ describe('Service: rentalService', function () {
 
     expect(result).toEqual(900);
   });
+
+  it('旧作を二日借りると 200 円', function () {
+    var result = rentalService.getRentalFee({type: 'regular'}, 2);
+
+    expect(result).toEqual(200);
+  });
+
+  it('旧作を三日借りると 350 円', function () {
+    var result = rentalService.getRentalFee({type: 'regular'}, 3);
+
+    expect(result).toEqual(350);
+  });
+
+  it('子供向けを三日借りると 300 円', function () {
+    var result = rentalService.getRentalFee({type: 'kids'}, 3);
+
+    expect(result).toEqual(300);
+  });
+
+  it('子供向けを四日借りると 450 円', function () {
+    var result = rentalService.getRentalFee({type: 'kids'}, 4);
+
+    expect(result).toEqual(450);
+  });
+
+  // テストコードをひたすら書く...
 });
